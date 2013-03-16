f0eb4c3689a4918c79ad4911ebcb66d1ea123b5c
tagsby <benhavey@gmail.com>
2013-03-16 16:29:19 -0400

    Commited changes made while off the internet

--- HW3/server.c
+++ HW3/server.c
@@ -9,7 +9,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void error(const char *msg){
+//Displays errors when a system call fails.
+void error(const char *msg) {
     perror(msg);
     exit(1);
 }
@@ -18,12 +19,24 @@ int main(int argc, char *argv[]) {
     int sockfd, newsockfd, portno;
     socklen_t clilen;
     char buffer[256];
+    //The sockaddr_in and serv_addr structures are defined in <sys/socket.h>
     struct sockaddr_in serv_addr, cli_addr;
+    //sockaddr_in contains sin_family, it needs to be AF_INET
+    //Clear sockaddr_in with memset
+    //sockaddr_in has: sin_family, sin_port, in_addr, sin_addr, sin_zero
+    //sin_family must be AF_INET, sin_port is the listening port, in_addr is IPv4
+    //network address. Idk wtf sin_addr does, sin_zero must stay = 0.
+
+   //serv_addr contains the address of the server.
+   //cli_addr contains the address of the client.
+
     int n;
+    //Checks to see if there was a provided port.
     if (argc < 2) {
         fprintf(stderr,"ERROR, no port provided\n");
         exit(1);
     }
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
