cab74e1e8ec13e5fc8da82189298f08addacc5c3
tagsby <benhavey@gmail.com>
2013-03-16 15:44:28 -0400

    Tidying up

--- HW3/examples/server/client.c
+++ HW3/examples/server/client.c
@@ -1,3 +1,4 @@
+//Stolen from www.linuxhowtos.org/C_C++/socket.html
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,10 +6,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 
-void error(const char *msg)
-{
+void error(const char *msg) {
     perror(msg);
     exit(0);
 }
@@ -21,12 +21,12 @@ int main(int argc, char *argv[])
 
     char buffer[256];
     if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        exit(0);
     }
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0)
         error("ERROR opening socket");
     server = gethostbyname(argv[1]);
     if (server == NULL) {
@@ -35,21 +35,20 @@ int main(int argc, char *argv[])
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr,
+      server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
     printf("Please enter the message: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
+    if (n < 0)
          error("ERROR writing to socket");
     bzero(buffer,256);
     n = read(sockfd,buffer,255);
-    if (n < 0) 
+    if (n < 0)
          error("ERROR reading from socket");
     printf("%s\n",buffer);
     close(sockfd);
