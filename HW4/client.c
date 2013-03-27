9e1d90adecbef7a25d8d943bfd080c0aa2b75ae5
tagsby <benhavey@gmail.com>
2013-03-27 19:09:00 -0400

    Added read in code for the client

--- HW4/client.c
+++ HW4/client.c
@@ -7,22 +7,20 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <ctype.h>
+#include "error.h"
 
 #ifndef BUF_SIZE
 #define BUF_SIZE 65536
 
-void error(const char *msg) {
-    perror(msg);
-    exit(0);
-}
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
     char buffer[BUF_SIZE];
+    char outbuf[BUF_SIZE]="0";
+
     if (argc < 3) {
         fprintf(stderr,"usage %s hostname port\n", argv[0]);
         exit(0);
@@ -43,16 +41,34 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,BUF_SIZE);
-    fgets(buffer,BUF_SIZE-1,stdin);
+    printf("Please type in as many numbers as you want. They must be positive integers, and followed by an enter. Any non-numeral characters will be ignored. Press enter to finish.\n");
+    int flipvar=1;
+    while (flipvar) {
+        bzero(buffer,BUF_SIZE);
+        fgets(buffer,BUF_SIZE-1,stdin);
+        printf("Number: %s",buffer);
+        flipvar=strcmp(buffer,"\n");
+        if (!strcmp(buffer,"exit\n")||!strcmp(buffer,"Exit\n")) {
+            printf("Exit signal sent to server.\n");
+            sprintf(outbuf,"ABORT"); //The server reads this as a "no add"
+            break;
+        }
+        buffer[strlen(buffer)-1]='\0';
+        if ((strlen(outbuf)+strlen(buffer)+3)>BUF_SIZE) {
+            printf("The current selection exceeds the server transfer limit. Either press enter to send the current numbers, or type exit to leave without sending anything.\n");
+            continue;
+        }
+        sprintf(outbuf,"%s %s",outbuf,buffer);
+    }
+    printf("Final string: %s.\n",outbuf);
+    return 0;
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0)
-         error("ERROR writing to socket");
+        error("ERROR writing to socket");
     bzero(buffer,BUF_SIZE);
     n = read(sockfd,buffer,BUF_SIZE-1);
     if (n < 0)
-         error("ERROR reading from socket");
+        error("ERROR reading from socket");
     printf("%s\n",buffer);
     close(sockfd);
     return 0;
--- HW4/server.c
+++ HW4/server.c
@@ -7,7 +7,7 @@
 #include <netinet/in.h>
 #include <time.h>
 #include "thread_run.c"
-#include "error.c"
+#include "error.h"
 
 #define BUF_SIZE 65536
 
@@ -15,11 +15,6 @@ void error(const char*);
 void *thread_run(void * arg);
 
 int main(int argc, char *argv[]) {
-    //Creates a sleep time. Server will only accept new requests every 1/10 of a second
-    struct timespec sleep_time;
-    sleep_time.tv_sec=0;
-    sleep_time.tv_nsec=100000000;
-
     pthread_attr_t attr;
     pthread_t threads;
     unsigned int ids;
@@ -27,26 +22,13 @@ int main(int argc, char *argv[]) {
     int sockfd, newsockfd, portno;
     socklen_t clilen;
     char buffer[BUF_SIZE];
-    //The sockaddr_in and serv_addr structures are defined in <sys/socket.h>
     struct sockaddr_in serv_addr, cli_addr;
-    //sockaddr_in contains sin_family, it needs to be AF_INET
-    //Clear sockaddr_in with memset
-    //sockaddr_in has: sin_family, sin_port, in_addr, sin_addr, sin_zero
-    //sin_family must be AF_INET, sin_port is the listening port, in_addr is IPv4
-    //network address. Idk wtf sin_addr does, sin_zero must stay = 0.
-
-    //serv_addr contains the address of the server.
-    //cli_addr contains the address of the client.
 
     //Checks to see if there was a provided port.
     if (argc < 2) {
         fprintf(stderr,"ERROR, no port provided\n");
         exit(1);
     }
-    //Socket function declared as: socket(domain,type,protocol);
-    //Domain: AF_INET represents IPv4 and AF_INET6 is IPv6
-    //Type: SOCK_STREAM is TCP, others are *_DGRAM, *_SEQPACKET, *_RAW
-    //Protocol: IPPROTO_TCP, *_SCTP, *_UDP, *_DCCP. 0 is the default for domain/type
 
     //Create a socket descriptor, confirm it's created.
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -57,16 +39,11 @@ int main(int argc, char *argv[]) {
     portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    //By binding to INADDR_ANY, our server doesn't need to know the IP address of the
-    //server before running. The bound socket will accept for the bound port that
-    //arrive on interfaces 0, 1, or 2. This locks the ammount of available interfaces
-    //for the port because you're taking up more interaces then necesary.
 
     //Set the server address' port to portno
     serv_addr.sin_port = htons(portno);
 
     //Bind the server address to the socket descriptor. Check if it was acceptable.
-//    bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR on binding");
     //Start listening for requests to the serer through the socket descriptor.
