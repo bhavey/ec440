f3e41e42703219676c0a0f6b5c441068edc262b3
tagsby <benhavey@gmail.com>
2013-04-10 10:48:49 -0800

    Adjusted bendev

--- HW5/Files/bendev.c
+++ HW5/Files/bendev.c
@@ -20,7 +20,7 @@ static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
 #define SUCCESS 0
-#define DEVICE_NAME "chardev" //Dev name as it appears in /proc/devices
+#define DEVICE_NAME "bendev" //Dev name as it appears in /proc/devices
 #define BUF_LEN 80 //Max length of the message from the device
 
 //Global variables are declared as static, so are global within the file.
