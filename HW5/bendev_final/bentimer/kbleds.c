e683c2c9e8b31ff71a05e2d68b5a687c0882e1b6
tagsby <benhavey@gmail.com>
2013-04-18 23:08:41 -0800

    Testing printing a message

--- HW5/bendev_final/bentimer/kbleds.c
+++ HW5/bendev_final/bentimer/kbleds.c
@@ -1,13 +1,13 @@
-/*
- * kbleds.c − Blink keyboard leds until the module is unloaded.(modified for 3.7.5)
- */
+//kbleds.c − Blink keyboard leds until the module is unloaded.
+//(modified for 3.7.5)
+
 #include <linux/module.h>
 #include <linux/configfs.h>
 #include <linux/init.h>
-#include <linux/tty.h>          /* For fg_console, MAX_NR_CONSOLES */
-#include <linux/kd.h>           /* For KDSETLED */
+#include <linux/tty.h>          //For fg_console, MAX_NR_CONSOLES
+#include <linux/kd.h>           //For KDSETLED
 #include <linux/vt.h>
-#include <linux/console_struct.h>       /* For vc_cons */
+#include <linux/console_struct.h>       //For vc_cons
 #include <linux/vt_kern.h>
 MODULE_DESCRIPTION("Example module illustrating the use of Keyboard LEDs.");
 MODULE_LICENSE("GPL");
@@ -17,8 +17,8 @@ char kbledstatus = 0;
 #define BLINK_DELAY   HZ/5
 #define ALL_LEDS_ON   0x07
 #define RESTORE_LEDS  0xFF
-/*
- * Function my_timer_func blinks the keyboard LEDs periodically by invoking
+
+/* Function my_timer_func blinks the keyboard LEDs periodically by invoking
  * command KDSETLED of ioctl() on the keyboard driver. To learn more on virtual
  * terminal ioctl operations, please see file:
  *     /usr/src/linux/drivers/char/vt_ioctl.c, function vt_ioctl().
@@ -29,38 +29,36 @@ char kbledstatus = 0;
  * the LEDs reflect the actual keyboard status). To learn more on this,
  * please see file:
  *     /usr/src/linux/drivers/char/keyboard.c, function setledstate().
- *
  */
-static void my_timer_func(unsigned long ptr)
-{
+//The blinking function.
+static void my_timer_func(unsigned long ptr) {
         int *pstatus = (int *)ptr;
-        if (*pstatus == ALL_LEDS_ON)
+        if (*pstatus == ALL_LEDS_ON) {
                 *pstatus = RESTORE_LEDS;
-        else
+        	printk(KERN_ERR "poop\n");
+        } else
                 *pstatus = ALL_LEDS_ON;
         (my_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED,
                             *pstatus);
         my_timer.expires = jiffies + BLINK_DELAY;
         add_timer(&my_timer);
 }
-static int __init kbleds_init(void)
-{
+
+static int __init kbleds_init(void) {
         int i;
-        printk(KERN_INFO "kbleds: loading\n");
-        printk(KERN_INFO "kbleds: fgconsole is %x\n", fg_console);
+        printk(KERN_ERR "kbleds: loading\n");
+        printk(KERN_ERR "kbleds: fgconsole is %x\n", fg_console);
         for (i = 0; i < MAX_NR_CONSOLES; i++) {
                 if (!vc_cons[i].d)
                         break;
-                printk(KERN_INFO "poet_atkm: console[%i/%i] #%i, tty %lx\n", i,
+                printk(KERN_ERR "poet_atkm: console[%i/%i] #%i, tty %lx\n", i,
                        MAX_NR_CONSOLES, vc_cons[i].d->vc_num,
                        (unsigned long)vc_cons[i].d->port.tty);
         }
-        printk(KERN_INFO "kbleds: finished scanning consoles\n");
+        printk(KERN_ERR "kbleds: finished scanning consoles\n");
         my_driver = vc_cons[fg_console].d->port.tty->driver;
-        printk(KERN_INFO "kbleds: tty driver magic %x\n", my_driver->magic);
-        /*
-         * Set up the LED blink timer the first time
-         */
+        printk(KERN_ERR "kbleds: tty driver magic %x\n", my_driver->magic);
+        //Set up the LED blink timer the first time
         init_timer(&my_timer);
         my_timer.function = my_timer_func;
         my_timer.data = (unsigned long)&kbledstatus;
@@ -68,9 +66,8 @@ static int __init kbleds_init(void)
         add_timer(&my_timer);
         return 0;
 }
-static void __exit kbleds_cleanup(void)
-{
-        printk(KERN_INFO "kbleds: unloading...\n");
+static void __exit kbleds_cleanup(void) {
+        printk(KERN_ERR "kbleds: unloading...\n");
         del_timer(&my_timer);
         (my_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED,
                             RESTORE_LEDS);
