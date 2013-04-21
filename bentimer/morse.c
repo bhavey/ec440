1dc7d0557aed125cd6956d5974454689b7b00d79
tagsby <benhavey@gmail.com>
2013-04-21 13:38:50 -0800

    started decoding process

--- bentimer/morse.c
+++ bentimer/morse.c
@@ -1,11 +1,11 @@
 //Morse code LED module for Raspberry Pi
-//static char *morse_table[] = {".-","-...","-.-.","-..", //A-D
-//    ".","..-.","--.","....","..",".---","-.-",".-..", //E-L
-//    "--","-.","---",".--.","--.-",".-.","...","-","..-", //M-U
-//    "...-",".--","-..-","-.--","--..", //V-Z
-//    ".----","..---","...--","....-",".....", //1-5
-//    "-....","--...","---..","----.","-----", //6-0
-//};
+static char *morse_table[] = {".-","-...","-.-.","-..", //A-D
+    ".","..-.","--.","....","..",".---","-.-",".-..", //E-L
+    "--","-.","---",".--.","--.-",".-.","...","-","..-", //M-U
+    "...-",".--","-..-","-.--","--..", //V-Z
+    ".----","..---","...--","....-",".....", //1-5
+    "-....","--...","---..","----.","-----", //6-0
+};
 
 #include <linux/module.h>
 #include <linux/configfs.h>
@@ -26,34 +26,40 @@ char morse_status = 0;
 #define DAH HZ*4/5 //dash
 #define SPACE HZ*2/5 //space
 
-//The dit function.
+//Note. Each time you set up a new timer, you need to consider how long
+//you want *that* timer to last. As in, you want to always set the LED
+//status for dots/dashes in the space preceeding them
 static void Dit(char morse_in) {
+	printk(KERN_ERR "Morse in: %c\n",morse_in);
 	timer_on=1; //Lock the function.
-        if (morse_status == 0) {
-		morse_status = 1;
-		printk(KERN_ERR "Dot\n");
-		my_timer.expires = jiffies + DAH; //Was blink delay
-		add_timer(&my_timer);
-	} else if (morse_status == 1) {
-		morse_status = 2;
-		printk(KERN_ERR "Off\n");
-		my_timer.expires = jiffies + SPACE; //Was blink delay
-		add_timer(&my_timer);
-	} else  if (morse_status == 2) {
-		printk(KERN_ERR "Done\n");
-		morse_status = 0;
-		timer_on=2;
-		printk(KERN_ERR "This is poop\n");
+	if (morse_table[97-morse_in][0]==".") {
+		morse_status = 6;
+		printk(KERN_ERR "We're here!\n");
+	} else {
+	        if (morse_status == 0) {
+			morse_status = 1;
+			printk(KERN_ERR "Dot\n");
+			my_timer.expires = jiffies + SPACE; //Was blink delay
+			add_timer(&my_timer);
+		} else if (morse_status == 1) {
+			morse_status = 2;
+			printk(KERN_ERR "Off\n");
+			my_timer.expires = jiffies + DAH; //Was blink delay
+			add_timer(&my_timer);
+		} else  if (morse_status == 2) {
+			printk(KERN_ERR "Done\n");
+			morse_status = 0;
+			timer_on=2;
+		}
 	}
 }
 
 static int __init kbleds_init(void) {
         printk(KERN_ERR "morse: loading\n");
-        my_driver = vc_cons[fg_console].d->port.tty->driver;
         init_timer(&my_timer);
         my_timer.function = Dit;
         my_timer.data = morse_in;
-        my_timer.expires = jiffies + DIT;
+        my_timer.expires = jiffies + DAH;
         add_timer(&my_timer);
 	printk(KERN_ERR "Got there!\n");
         return 0;
