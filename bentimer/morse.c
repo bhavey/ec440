4ae1caac2b85df2035002083b55b1e821847783e
tagsby <benhavey@gmail.com>
2013-04-21 12:03:52 -0800

    Cleaned up misc code

--- bentimer/morse.c
+++ bentimer/morse.c
@@ -17,33 +17,19 @@ char kbledstatus = 0;
 #define DIT HZ/5 //dot
 #define DAH HZ*4/5 //dash
 #define SPACE HZ*2/5 //space
-#define ALL_LEDS_ON 0x07
-#define RESTORE_LEDS 0xFF
 
-/* Function my_timer_func blinks the keyboard LEDs periodically by invoking
-* command KDSETLED of ioctl() on the keyboard driver. To learn more on virtual
-* terminal ioctl operations, please see file:
-* /usr/src/linux/drivers/char/vt_ioctl.c, function vt_ioctl().
-*
-* The argument to KDSETLED is alternatively set to 7 (thus causing the led
-* mode to be set to LED_SHOW_IOCTL, and all the leds are lit) and to 0xFF
-* (any value above 7 switches back the led mode to LED_SHOW_FLAGS, thus
-* the LEDs reflect the actual keyboard status). To learn more on this,
-* please see file:
-* /usr/src/linux/drivers/char/keyboard.c, function setledstate().
-*/
-//The blinking function.
+//The dit function.
 static void my_timer_func(unsigned long ptr) {
         int *pstatus = (int *)ptr;
         if (*pstatus == 0) {
                 *pstatus = 1;
 		printk(KERN_ERR "On\n");
-		my_timer.expires = jiffies + LONG_DELAY; //Was blink delay
+		my_timer.expires = jiffies + DAH; //Was blink delay
 		add_timer(&my_timer);
 	} else if (*pstatus == 1) {
 		printk(KERN_ERR "Off\n");
 		*pstatus = 2;
-		my_timer.expires = jiffies + SHORT_DELAY; //Was blink delay
+		my_timer.expires = jiffies + SPACE; //Was blink delay
 		add_timer(&my_timer);
 	}
 }
@@ -62,11 +48,10 @@ static int __init kbleds_init(void) {
         printk(KERN_ERR "kbleds: finished scanning consoles\n");
         my_driver = vc_cons[fg_console].d->port.tty->driver;
         printk(KERN_ERR "kbleds: tty driver magic %x\n", my_driver->magic);
-        //Set up the LED blink timer the first time
         init_timer(&my_timer);
         my_timer.function = my_timer_func;
         my_timer.data = (unsigned long)&kbledstatus;
-        my_timer.expires = jiffies + BLINK_DELAY;
+        my_timer.expires = jiffies + DIT;
         add_timer(&my_timer);
         return 0;
 }
