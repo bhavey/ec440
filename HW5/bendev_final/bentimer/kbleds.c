0114ce3eeaeca3e0e7587e14df5c6980d6dcb6a5
tagsby <benhavey@gmail.com>
2013-04-21 11:40:33 -0800

    Back in the saddle again.

--- HW5/bendev_final/bentimer/kbleds.c
+++ HW5/bendev_final/bentimer/kbleds.c
@@ -1,60 +1,52 @@
 //kbleds.c − Blink keyboard leds until the module is unloaded.
 //(modified for 3.7.5)
 //Really cool. If you're using a Mac it just toggles caps locks :D
-#include <linux/timer.h>
+
+
 #include <linux/module.h>
 #include <linux/configfs.h>
 #include <linux/init.h>
-#include <linux/tty.h>          //For fg_console, MAX_NR_CONSOLES
-#include <linux/kd.h>           //For KDSETLED
+#include <linux/tty.h> //For fg_console, MAX_NR_CONSOLES
+#include <linux/kd.h> //For KDSETLED
 #include <linux/vt.h>
-#include <linux/console_struct.h>       //For vc_cons
+#include <linux/console_struct.h> //For vc_cons
 #include <linux/vt_kern.h>
 MODULE_DESCRIPTION("Example module illustrating the use of Keyboard LEDs.");
 MODULE_LICENSE("GPL");
 struct timer_list my_timer;
 struct tty_driver *my_driver;
 char kbledstatus = 0;
-bool done_now = 0;
-
-//static void mykmod_work_handler(struct work_struct *w);
-//static struct workqueue_struct *wq = 0;
-//static DECLARE_DELAYED_WORK(mykmod_work, mykmod_work_handler);
-//static unsigned long onesec;
-
-#define BLINK_DELAY   HZ/5
-#define SHORT_DELAY   HZ
-#define ALL_LEDS_ON   0x07
-#define RESTORE_LEDS  0xFF
+#define BLINK_DELAY HZ/5
+#define SHORT_DELAY HZ
+#define ALL_LEDS_ON 0x07
+#define RESTORE_LEDS 0xFF
 
 /* Function my_timer_func blinks the keyboard LEDs periodically by invoking
- * command KDSETLED of ioctl() on the keyboard driver. To learn more on virtual
- * terminal ioctl operations, please see file:
- *     /usr/src/linux/drivers/char/vt_ioctl.c, function vt_ioctl().
- *
- * The argument to KDSETLED is alternatively set to 7 (thus causing the led
- * mode to be set to LED_SHOW_IOCTL, and all the leds are lit) and to 0xFF
- * (any value above 7 switches back the led mode to LED_SHOW_FLAGS, thus
- * the LEDs reflect the actual keyboard status). To learn more on this,
- * please see file:
- *     /usr/src/linux/drivers/char/keyboard.c, function setledstate().
- */
+* command KDSETLED of ioctl() on the keyboard driver. To learn more on virtual
+* terminal ioctl operations, please see file:
+* /usr/src/linux/drivers/char/vt_ioctl.c, function vt_ioctl().
+*
+* The argument to KDSETLED is alternatively set to 7 (thus causing the led
+* mode to be set to LED_SHOW_IOCTL, and all the leds are lit) and to 0xFF
+* (any value above 7 switches back the led mode to LED_SHOW_FLAGS, thus
+* the LEDs reflect the actual keyboard status). To learn more on this,
+* please see file:
+* /usr/src/linux/drivers/char/keyboard.c, function setledstate().
+*/
 //The blinking function.
 static void my_timer_func(unsigned long ptr) {
         int *pstatus = (int *)ptr;
         if (*pstatus == 0) {
                 *pstatus = 1;
-        	printk(KERN_ERR "On\n");
-        	my_timer.expires = jiffies + SHORT_DELAY; //Was blink delay
-	        add_timer(&my_timer);
+         printk(KERN_ERR "On\n");
+         my_timer.expires = jiffies + SHORT_DELAY; //Was blink delay
+add_timer(&my_timer);
         } else if (*pstatus == 1) {
                 printk(KERN_ERR "Off\n");
                 *pstatus = 2;
-        	my_timer.expires = jiffies + BLINK_DELAY; //Was blink delay
-        	add_timer(&my_timer);
-	} else {
-		done_now=1;
-	}
+         my_timer.expires = jiffies + BLINK_DELAY; //Was blink delay
+         add_timer(&my_timer);
+}
 }
 
 static int __init kbleds_init(void) {
@@ -77,15 +69,13 @@ static int __init kbleds_init(void) {
         my_timer.data = (unsigned long)&kbledstatus;
         my_timer.expires = jiffies + BLINK_DELAY;
         add_timer(&my_timer);
-	while (timer_pending(&my_timer));
-	printk(KERN_ERR "Done!\n");
-//	wait_event_interruptible(data,
-
         return 0;
 }
 static void __exit kbleds_cleanup(void) {
         printk(KERN_ERR "kbleds: unloading...\n");
         del_timer(&my_timer);
+// (my_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED,
+// RESTORE_LEDS);
 }
 module_init(kbleds_init);
 module_exit(kbleds_cleanup);
