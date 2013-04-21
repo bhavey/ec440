3ba41d3ebdea7bc8102a79e595e6e6ec73d5b311
tagsby <benhavey@gmail.com>
2013-04-21 13:25:29 -0800

    Please dont be totally broken

--- bentimer/morse.c
+++ bentimer/morse.c
@@ -1,11 +1,11 @@
 //Morse code LED module for Raspberry Pi
-static char *morse_table[] = {".-","-...","-.-.","-..", //A-D
-    ".","..-.","--.","....","..",".---","-.-",".-..", //E-L
-    "--","-.","---",".--.","--.-",".-.","...","-","..-", //M-U
-    "...-",".--","-..-","-.--","--..", //V-Z
-    ".----","..---","...--","....-",".....", //1-5
-    "-....","--...","---..","----.","-----", //6-0
-};
+//static char *morse_table[] = {".-","-...","-.-.","-..", //A-D
+//    ".","..-.","--.","....","..",".---","-.-",".-..", //E-L
+//    "--","-.","---",".--.","--.-",".-.","...","-","..-", //M-U
+//    "...-",".--","-..-","-.--","--..", //V-Z
+//    ".----","..---","...--","....-",".....", //1-5
+//    "-....","--...","---..","----.","-----", //6-0
+//};
 
 #include <linux/module.h>
 #include <linux/configfs.h>
@@ -20,43 +20,40 @@ MODULE_LICENSE("GPL");
 struct timer_list my_timer;
 int timer_on = 0;
 struct tty_driver *my_driver;
+char morse_in = 'a';
 char morse_status = 0;
 #define DIT HZ/5 //dot
 #define DAH HZ*4/5 //dash
 #define SPACE HZ*2/5 //space
 
 //The dit function.
-static void Dit(unsigned long ptr) {
+static void Dit(char morse_in) {
 	timer_on=1; //Lock the function.
-        int *pstatus = (int *)ptr;
-        if (*pstatus == 0) {
-		*pstatus = 1;
+        if (morse_status == 0) {
+		morse_status = 1;
 		printk(KERN_ERR "Dot\n");
 		my_timer.expires = jiffies + DAH; //Was blink delay
 		add_timer(&my_timer);
-	} else if (*pstatus == 1) {
+	} else if (morse_status == 1) {
 		printk(KERN_ERR "Off\n");
-		*pstatus = 2;
 		my_timer.expires = jiffies + SPACE; //Was blink delay
 		add_timer(&my_timer);
-	} else  if (*pstatus == 2) {
+	} else  if (morse_status == 2) {
 		printk(KERN_ERR "Done\n");
-		*pstatus = 0;
+		morse_status = 0;
 		timer_on=2;
 		printk(KERN_ERR "This is poop\n");
 	}
 }
 
 static int __init kbleds_init(void) {
-	bool this_is_stupid=1;
         printk(KERN_ERR "morse: loading\n");
         my_driver = vc_cons[fg_console].d->port.tty->driver;
         init_timer(&my_timer);
         my_timer.function = Dit;
-        my_timer.data = (unsigned long)&morse_status;
+        my_timer.data = morse_in;
         my_timer.expires = jiffies + DIT;
         add_timer(&my_timer);
-	this_is_stupid=1;
 	printk(KERN_ERR "Got there!\n");
         return 0;
 }
